750460e3c265dac7594e1eb462258da0dc42da0f
verminouszeus <90755033+verminouszeus@users.noreply.github.com>
2023-02-04 13:58:02 +0100

    update map 2

--- Roots Game Jam/Assets/thuus/scripts/gamemaster.cs
+++ Roots Game Jam/Assets/thuus/scripts/gamemaster.cs
@@ -6,18 +6,30 @@ public class gamemaster : MonoBehaviour
 {
     public int maxpuzzlepoints;
     public int puzzle2Points;
+    public Animator animate;
+
     void Start()
     {
         
     }
 
+    IEnumerator Order()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
     void Update()
     {
 
-        if (puzzle2Points == maxpuzzlepoints)
+        if (puzzle2Points >= maxpuzzlepoints)
         {
-            Destroy(gameObject);
+            
+            animate.SetBool("bush",true);
+                Order();
             Debug.Log("destroyed");
+
+
         }
     }
+
 }
