9862e7e93518986985c7f53bbefff1119a8dd545
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2023-02-05 07:55:44 +0100

    definitfe animations

--- Roots Game Jam/Assets/thuus/trancition.cs
+++ Roots Game Jam/Assets/thuus/trancition.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class trancition : MonoBehaviour
 {
-    public int scene;
+    public string scene;
     public Animator animate;
+    public Animator animator;
+    public GameObject player;
     void Start()
     {
-        
+        animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        animate.SetBool("transition", true);
-        StartCoroutine(Waitbeforedestroyy());    }
-    
+        player.GetComponent<PlayerController>().speed = 0;
+        StartCoroutine(PlayerAnimation()); 
+
+    }
+    IEnumerator PlayerAnimation()
+    {
+        animator.SetBool("tree", true);
+        yield return new WaitForSeconds(4);
+        StartCoroutine(Waitbeforedestroyy());
+    }
+
     IEnumerator Waitbeforedestroyy()
     {
+        animate.SetBool("transition", true);
         yield return new WaitForSeconds(3);
         Debug.Log("z");
-        SceneManager.LoadScene(sceneBuildIndex: scene);
+        SceneManager.LoadScene(scene);
     }
 
 }
